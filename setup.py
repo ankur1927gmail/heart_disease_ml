32087286005cebc0558a39651d21830942c91bc3
Ankur Sharma <ankur.1927@gmail.com>
2026-07-01 10:09:46 +0530

    changes made to setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Suchita',
-author_email='singhsuchita4@gmail.com',
+author='Ankur Sharma',
+author_email='ankur.1927@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
